597c0a98628c3be0f7e055094f423f57408fbac3
Pavel Popov <legeon48@mail.ru>
2022-05-08 14:06:45 +0300

    Update

--- Produser/Program.cs
+++ Produser/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +12,27 @@ namespace Produser
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
+        private static IPEndPoint consumerEndPoint;
+       [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            // Application.Run(new Form1());
+            var consumerIp = ConfigurationManager.AppSettings.Get("consumerIp");
+            var consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIp), consumerPort);
+            //Console.WriteLine($"consumer: {consumerEndPoint}");
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            
+        }
+
+        private static void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            throw new NotImplementedException();
         }
     }
 }
